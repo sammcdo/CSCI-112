d79fbbf97b12aa9d893bf3fc06fb3eddbab78b58
_____ _______ <sam_mcdo@yahoo.com>
2020-07-02 08:09:32 -0700

    fixed weird lab 1 error

---
+++ helloWorld/lab1p1/lab1p1.c
@@ -0,0 +1,13 @@
+/*
+A basic introductory program that outputs text to the console.
+Sam McDowell 6.24.2020
+*/
+
+#include <stdio.h>	//Include the definition of printf in this code.
+
+//define the main function
+int main(void) {
+	//Use printf to output to the console.
+	printf("Hello amazing world of C!\n");
+	return 0; //End the program with error code 0 (success)
+}
